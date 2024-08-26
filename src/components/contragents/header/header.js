e4791e209289b6404ff0a8d5780dee642373e8b6
ysemyk <ysemyk@moysklad.ru>
2024-08-26 19:54:59 +0400

    Компонент хедера

--- src/components/contragents/header/header.js
+++
@@ -1,8 +0,0 @@
-import html from "./header.html";
-import './header.css';
-
-const element = document.createElement('div');
-element.innerHTML = html;
-element.classList.add("page-header");
-
-export default () => element;
---
+++ src/components/header/header.tsx
@@ -0,0 +1,18 @@
+import * as css from './header.module.css'
+import logo from '../../assets/logo.svg';
+import addIcon from '../../assets/add_data.svg';
+
+export default () => {
+
+    return (
+        <div className={css.page_header}>
+            <img className={css.header_left} src={logo}></img>
+            <div className={css.header_middle}></div>
+            <button type="button"
+                    className={css.header_right}>
+                <img className={css.add_data_img} src={addIcon}></img>
+                <div className={css.add_data_div}>Создать</div>
+            </button>
+        </div>
+    )
+}
